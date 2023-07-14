904195ab7a806f26e5c07c2d01d985eb68ab8c97
Randy <quaceyrandy@gmail.com>
2023-07-14 13:55:03 -0700

    I really hope I use this in the future

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print combinations of 0 to 9
+ *		with putchar separated by , and space
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(',');
+		}
+		++digit;
+	}
+	putchar('\n');
+
+	return (0);
+}
